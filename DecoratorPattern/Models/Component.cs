fec4fd6fada1ef59f9b64819bb69d8f27db4d98e
Back-End.07 <sonita.borin2@gmail.com>
2024-05-13 14:25:37 +0700

    Init Directory

---
+++ DecoratorPattern/Models/Component.cs
@@ -0,0 +1,47 @@
+﻿namespace DecoratorPattern.Models
+{
+    public abstract class Component
+    {
+        public abstract void Operation();
+    }
+    public class ConcreteComponent : Component
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteComponent.Operation()");
+        }
+    }
+    public abstract class Decorator : Component
+    {
+        protected Component _component = null!;
+        public void SetComponent(Component component) => _component = component;
+        public override void Operation()
+        {
+            if(_component != null)
+            {
+                _component.Operation();
+            }
+        }
+    }
+    public class ConcreteDecoratorA : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            Console.WriteLine("Concrete Decorator A");
+        }
+    }
+    public class ConcreteDecoratorB : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            Console.WriteLine("Concrete Decorator B");
+            AddedBehavior();
+        }
+        void AddedBehavior()
+        {
+            Console.WriteLine("Added Behavior in ConcreteDecorator B");
+        }
+    }
+}
---
+++ DecoratorPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿    using DecoratorPattern.Models;
+
+    var concreteClass = new ConcreteComponent();
+    var decoratorA = new ConcreteDecoratorA();
+    var decoratorB = new ConcreteDecoratorB();
+
+    decoratorA.SetComponent(concreteClass);
+    decoratorB.SetComponent(decoratorA);
+
+    decoratorB.Operation();
+
+    Console.ReadKey();
