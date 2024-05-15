8da68c32603783f239aefb1baf68078bffb522d7
Back-End.07 <sonita.borin2@gmail.com>
2024-05-15 16:45:58 +0700

    Init new patterns

---
+++ AdapterPattern/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace AdapterPattern
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Salary { get; set; }
+    }
+}
---
+++ AdapterPattern/ThirdPartyBillingSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    public class ThirdPartyBillingSystem
+    {
+        public void ProcessSalary(List<Employee> employees)
+        {
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"EUR {employee.Salary} salary credited to {employee.Name}'s account.");
+            }
+        }
+    }
+}
